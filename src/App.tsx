c786a0a0e94ab9401c5e6f5786499a1aa5328031
J Perrin <perrinjack96@gmail.com>
2020-10-08 15:28:56 +0100

    Added some basic styling I found online, N.B alot is redundant

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './App.scss';
 
 import Header from './components/header';
 function App() {
   return (
-    <div className="App">
+    <div className="app-body">
       <Header />
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import HeaderInfo from './headerInfo';
+import '../styles/header.scss';
 const Header = () => {
-  return <HeaderInfo itemText="Jack Perrin" />;
+  return (
+    <div className="header">
+      <div className="header-details">
+        {' '}
+        <HeaderInfo itemText="Jack Perrin" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
