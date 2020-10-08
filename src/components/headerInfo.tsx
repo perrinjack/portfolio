ab6488ba267672aae2cf824f8941977b639d2946
J Perrin <perrinjack96@gmail.com>
2020-10-08 16:15:16 +0100

    Font awesome installed, User icon displayed in headerinfo component

--- src/components/headerInfo.tsx
+++ src/components/headerInfo.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import '../styles/header.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+library.add(faUser);
 type HeaderInfoProps = {
   itemText: string;
   link?: string;
 };
 const HeaderInfo = ({ itemText, link }: HeaderInfoProps) => {
-  return <div>{link ? <a href = {link}> {itemText} </a> : <div>{itemText}</div>}</div>;
+  return (
+    <div>
+      <div className="header-detail">
+        {' '}
+        <div>
+          <FontAwesomeIcon icon="user" className="icon" />
+        </div>
+        {link ? <a href={link}> {itemText} </a> : <div>{itemText}</div>}
+      </div>
+    </div>
+  );
 };
 
 export default HeaderInfo;
