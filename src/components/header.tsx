62e8d16aaa6188a3aba8597f94d4a73c815e87ae
J Perrin <perrinjack96@gmail.com>
2020-10-08 15:51:10 +0100

    headerInfo has links for linkedIn & github

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,6 +13,12 @@ const Header = () => {
       </div>
       <div className="header-details">
         <HeaderInfo itemText="Jack Perrin" />
+        <HeaderInfo itemText="Full Stack Developer" />
+        <HeaderInfo
+          itemText="LinkedIn"
+          link="https://www.linkedin.com/in/perrinjack/"
+        />
+        <HeaderInfo itemText="Github" link="https://github.com/perrinjack" />
       </div>
     </div>
   );
--- src/components/headerInfo.tsx
+++ src/components/headerInfo.tsx
@@ -3,9 +3,10 @@ import '../styles/header.scss';
 
 type HeaderInfoProps = {
   itemText: string;
+  link?: string;
 };
-const HeaderInfo = ({ itemText }: HeaderInfoProps) => {
-  return <h1>{itemText}</h1>;
+const HeaderInfo = ({ itemText, link }: HeaderInfoProps) => {
+  return <div>{link ? <a href = {link}> {itemText} </a> : <div>{itemText}</div>}</div>;
 };
 
 export default HeaderInfo;
