25c1a5fea97ed7da1456da837dc10f43b7b0d823
J Perrin <perrinjack96@gmail.com>
2020-10-08 15:36:55 +0100

    Profile picture added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,14 @@ import '../styles/header.scss';
 const Header = () => {
   return (
     <div className="header">
+      <div>
+        <img
+          className="profile-photo"
+          src={'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'}
+          alt="Jack Perrin"
+        />
+      </div>
       <div className="header-details">
-        {' '}
         <HeaderInfo itemText="Jack Perrin" />
       </div>
     </div>
