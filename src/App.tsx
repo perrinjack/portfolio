d2d30c07cbb34a8ccbcb6f1c0abfe431b414457e
J Perrin <perrinjack96@gmail.com>
2020-10-09 13:58:47 +0100

    Added links on Navbar to correct locations

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,48 @@ import { BottomNavigation } from '@material-ui/core';
 import { BottomNavigationAction } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/">
-            <div>
-              <div className="app-body">
-                <Header />
+class App extends React.Component {
+  handlePhoneClick = () => (event: any) => {
+    window.open('tel:+447590606244');
+  };
+  handleMailClick = () => (event: any) => {
+    window.open('mailto:perrinjack96@gmail.com');
+  };
 
-                <div className="experience-container">
-                  <Experience title="Makers Academy" summary="blah blah" />
-                  <Experience title="Bristol University" summary="blah blah" />
-                  <Experience title="St Albans School" summary="blah blah" />
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/">
+              <div>
+                <div className="app-body">
+                  <Header />
+
+                  <div className="experience-container">
+                    <Experience title="Makers Academy" summary="blah blah" />
+                    <Experience
+                      title="Bristol University"
+                      summary="blah blah"
+                    />
+                    <Experience title="St Albans School" summary="blah blah" />
+                  </div>
                 </div>
-              </div>
-              <BottomNavigation showLabels={true}>
-                <BottomNavigationAction
-                  label="Call"
-                  component={Link}
-                  to="/signal"
-                />
-                <BottomNavigationAction label="Mail" />
-                <BottomNavigationAction label="LinkedIn" />
-              </BottomNavigation>
-            </div>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+                <BottomNavigation showLabels={true}>
+                  <BottomNavigationAction
+                    label="Call"
+                    onClick={this.handlePhoneClick()}
+                  />
 
+                  <BottomNavigationAction label="Mail" onClick={this.handleMailClick()}/>
+                  <BottomNavigationAction label="LinkedIn" />
+                </BottomNavigation>
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 export default App;
